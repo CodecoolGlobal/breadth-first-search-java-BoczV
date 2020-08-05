3539a55b1e0f9fce59ce667177f6c17b6a30eb68
Viktor Bocz <beviktor95@gmail.com>
2020-08-05 23:30:06 +0200

    Checks of the first functionality

--- src/main/java/com/codecool/bfsexample/BFSExample.java
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -1,6 +1,8 @@
 package com.codecool.bfsexample;
 
 import com.codecool.bfsexample.model.UserNode;
+import com.codecool.bfsexample.searchFunctions.bfs.BreadthFirstSearch;
+
 import java.util.List;
 
 public class BFSExample {
@@ -11,7 +13,14 @@ public class BFSExample {
         List<UserNode> users = generator.generate();
 
         GraphPlotter graphPlotter = new GraphPlotter(users);
-        
+
+        BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
+
+        UserNode userFrom = users.get(generator.randomGenNumber(users));
+        UserNode userTo = users.get(generator.randomGenNumber(users));
+        System.out.println("Minimum distance between " + userFrom.getFirstName() + " " + userFrom.getLastName() + "(" + userFrom.getId() + ")"
+                + " and " + userTo.getFirstName() + " " + userTo.getLastName() + "(" + userTo.getId() + ")" + " was: "
+                + breadthFirstSearch.returnMinimumDistance(userFrom, userTo));
         System.out.println("Done!");
     }
 
