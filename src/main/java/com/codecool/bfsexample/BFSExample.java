086b5161a21419fc5a8b8b6273160b1301e86d5b
Viktor Bocz <beviktor95@gmail.com>
2020-08-07 22:49:08 +0200

    All of the three functionality has finalized

--- src/main/java/com/codecool/bfsexample/BFSExample.java
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -4,6 +4,7 @@ import com.codecool.bfsexample.model.UserNode;
 import com.codecool.bfsexample.searchFunctions.bfs.BreadthFirstSearch;
 
 import java.util.List;
+import java.util.Set;
 
 public class BFSExample {
 
@@ -13,19 +14,38 @@ public class BFSExample {
         List<UserNode> users = generator.generate();
 
         GraphPlotter graphPlotter = new GraphPlotter(users);
-        checkFirstFunctionality(users, generator);
+        BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
 
+        /*checkFirstFunctionality(breadthFirstSearch, users, generator);
+        checkSecondFunctionality(breadthFirstSearch, users, generator, 3);*/
+
+        checkThirdFunctionality(breadthFirstSearch, users, generator, 1);
         System.out.println("Done!");
     }
 
-    private static void checkFirstFunctionality(List<UserNode> users, RandomDataGenerator generator){
-        BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
+    private static void checkFirstFunctionality(BreadthFirstSearch breadthFirstSearch, List<UserNode> users, RandomDataGenerator generator){
 
         UserNode userFrom = users.get(generator.randomGenNumber(users));
         UserNode userTo = users.get(generator.randomGenNumber(users));
         System.out.println("Minimum distance between " + userFrom.getFirstName() + " " + userFrom.getLastName() + "(" + userFrom.getId() + ")"
                 + " and " + userTo.getFirstName() + " " + userTo.getLastName() + "(" + userTo.getId() + ")" + " was: "
                 + breadthFirstSearch.returnMinimumDistance(userFrom, userTo));
+    }
+
+    private static void checkSecondFunctionality(BreadthFirstSearch breadthFirstSearch, List<UserNode> users, RandomDataGenerator generator, int distance){
+        UserNode userFrom = users.get(generator.randomGenNumber(users));
+        Set<UserNode> results = breadthFirstSearch.returnFriendsOfFriendsAtALevel(userFrom, distance);
+        System.out.println(userFrom.getFirstName() + " " + userFrom.getLastName() + "(" + userFrom.getId() + "): " + results);
+    }
+
+
+    private static void checkThirdFunctionality(BreadthFirstSearch breadthFirstSearch, List<UserNode> users, RandomDataGenerator generator, int distance){
+        UserNode userFrom = users.get(generator.randomGenNumber(users));
+        UserNode userTo = users.get(generator.randomGenNumber(users));
+        List<UserNode> result = breadthFirstSearch.returnShortestPath(userFrom, userTo, distance);
+        System.out.println("Shortest path between " + userFrom.getFirstName() + " "
+                + userFrom.getLastName() + "(" + userFrom.getId() + ") and " + userTo.getFirstName() + " "
+                + userTo.getLastName() + "(" + userTo.getId() + ") is " + result);
     }
 
     public static void main(String[] args) {
--- src/main/java/com/codecool/bfsexample/searchFunctions/bfs/BreadthFirstSearch.java
+++ src/main/java/com/codecool/bfsexample/searchFunctions/bfs/BreadthFirstSearch.java
@@ -2,10 +2,8 @@ package com.codecool.bfsexample.searchFunctions.bfs;
 
 import com.codecool.bfsexample.model.UserNode;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BreadthFirstSearch {
 
@@ -13,33 +11,94 @@ public class BreadthFirstSearch {
 
     public int returnMinimumDistance(UserNode userFrom, UserNode userTo){
         roundNumber = 0;
-        checkFriends(userFrom.getFriends(), userTo);
+        Set<UserNode> visited = new HashSet<>();
+        visited.add(userFrom);
+        checkFriends(userFrom.getFriends(), userTo, visited);
         return roundNumber;
     }
 
-    private boolean checkFriends(Set<UserNode> friends, UserNode userTo){
+    private boolean checkFriends(Set<UserNode> friends, UserNode userTo, Set<UserNode> visited){
         roundNumber++;
         Set<UserNode> friendsOfFriends = new HashSet<>();
         for(UserNode friend: friends){
-            if(friend == userTo){
-                return true;
-            } else {
-                friendsOfFriends.addAll(friend.getFriends());
+            if(!visited.contains(friend)){
+                if(friend == userTo){
+                    return true;
+                } else {
+                    visited.add(friend);
+                    friendsOfFriends.addAll(friend.getFriends());
+                }
             }
         }
-        return checkFriends(friendsOfFriends, userTo);
+        return checkFriends(friendsOfFriends, userTo, visited);
     }
-
 
 
 
     public Set<UserNode> returnFriendsOfFriendsAtALevel(UserNode userNode, int distance){
-        return new HashSet<UserNode>();
+        roundNumber = 1;
+        Set<UserNode> friendsOfFriends = userNode.getFriends();
+        Set<UserNode> visitedFriends = new HashSet<>();
+        visitedFriends.add(userNode);
+        friendsOfFriends = updateFriendsOfFriendsList(friendsOfFriends, visitedFriends, distance);
+        return friendsOfFriends.stream().filter(person -> !visitedFriends.contains(person)).collect(Collectors.toSet());
+    }
+
+    private Set<UserNode> updateFriendsOfFriendsList(Set<UserNode> users, Set<UserNode> visitedFriends, int distance){
+        Set<UserNode> newFriends = new HashSet<>();
+        if(roundNumber == distance) {
+            return users;
+        }
+        for(UserNode friend: users){
+            newFriends.addAll(friend.getFriends());
+            visitedFriends.add(friend);
+        }
+        roundNumber++;
+        users = newFriends;
+        return updateFriendsOfFriendsList(users, visitedFriends, distance);
     }
 
 
+
+
+
     public List<UserNode> returnShortestPath(UserNode userFrom, UserNode userTo, int distance){
-        return new ArrayList<>();
+        List<UserNode> allPeople = new ArrayList<>();
+        allPeople.add(userFrom);
+        allPeople.addAll(userFrom.getFriends());
+        exploreAllListsOfPeople(allPeople, userFrom.getFriends(), userTo, userFrom);
+
+        allPeople = allPeople.stream().distinct().collect(Collectors.toList());
+        List<UserNode> shortestPath = findShortestPath(allPeople, userTo);
+
+        shortestPath.add(userFrom);
+        Collections.reverse(shortestPath);
+        return shortestPath;
+    }
+
+    private boolean exploreAllListsOfPeople(List<UserNode> allPeople, Set<UserNode> friends, UserNode userTo, UserNode userFrom){
+        Set<UserNode> friendsInTheSameLevel = new HashSet<>();
+        for(UserNode friend: friends){
+            if(friend.getFriends().contains(userTo)){
+                return true;
+            }
+            friendsInTheSameLevel.addAll(friend.getFriends().stream().filter(element -> !element.equals(userFrom))
+                    .collect(Collectors.toSet()));
+        }
+        allPeople.addAll(friendsInTheSameLevel);
+        return exploreAllListsOfPeople(allPeople, friendsInTheSameLevel, userTo, userFrom);
+    }
+
+    private List<UserNode> findShortestPath(List<UserNode> allPeople, UserNode userTo){
+        List<UserNode> shortestPath = new ArrayList<>();
+        UserNode helperNode = userTo;
+        for(int i = allPeople.size() - 1; i >= 0; i--){
+            if(allPeople.get(i).getFriends().contains(helperNode)){
+                shortestPath.add(helperNode);
+                helperNode = allPeople.get(i);
+            }
+        }
+        return shortestPath;
     }
 
 }
